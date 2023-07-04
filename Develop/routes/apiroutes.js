27ed58f317e1322923133df40fa2658f5340a401
Rajni bala <balatamna16@gmail.com>
2023-07-04 16:02:40 -0400

    get request

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -3,4 +3,15 @@ const util = require('util');
 const router = require('express').Router();
 const writeFileAsync = util.promisify(fs.writeFile);
 const readFileAsync = util.promisify(fs.readFile);
-var notesData
+var notesData;
+
+//GET REQUEST
+router.get('api/notes',(req,res)=>{
+  readFileAsync('db/db.json','utf8').then(function(data){
+    notesData = JSON.parse(data);
+    res.json(notesData);
+  });
+
+});
+
+//POST REQUEST
