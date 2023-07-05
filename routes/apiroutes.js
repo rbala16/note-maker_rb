a3094db3c44c5a07ce0e48bd1b21f17bb6a5a5e2
Rajni bala <balatamna16@gmail.com>
2023-07-05 15:19:04 -0400

    README created

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -17,16 +17,37 @@ app.get('/notes',(req,res)=>{
 //POST REQUEST
 app.post('/notes',(req,res)=>{
   readFileAsync('db/db.json','utf8').then(function(data){
+    //parse data 
     notesData = JSON.parse(data);
     let createNewnote = req.body;
     let currentID = notesData.length;
     createNewnote.id = currentID + 1;
     notesData.push(createNewnote);
+    notesData = JSON.stringify(notesData);
     writeFileAsync("db/db.json", notesData).then(function (data) {
-      console.log("Note has been added.");
+      console.log("Note has been created.");
     });
     res.json(notesData);
   });
 });
+
+// DELETE request
+app.delete("/notes/:id", (req, res) => {
+  let selID = parseInt(req.params.id);
+  //  Read JSON file
+  for (let i = 0; i < notesData.length; i++) {
+    if (selID === notesData[i].id) {
+      notesData.splice(i, 1);
+      let noteJSON = JSON.stringify(notesData, null, 2);
+
+      writeFileAsync("db/db.json", noteJSON).then(function () {
+        console.log("Note has been deleted.");
+      });
+    }
+  }
+  res.json(notesData);
+});
+
+
 
 module.exports = app;
--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const router = require('express').Router();
 
+//Get request
 router.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
+//Go to homepage
 router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
